4aa836a57a448f7dbae5d23ad047475043da1c54
gerardllorach <gerard.llorach@gmail.com>
2021-04-19 14:50:24 +0200

    vue voicemap

--- voiceMap.js
+++ voiceMap.js
@@ -11,50 +11,55 @@ export function voiceMapUpdate(canvas, inCanvasCtx, xMouse, yMouse, mouseState){
   canvasCtx = inCanvasCtx;
 
   // Draw 2D interface
-  var sizeR = 200;
-  var wposW = 30 + sizeR;
-  var wposH = canvas.height/3 + sizeR;
+  var sizeR = canvas.width;
 
-  canvasCtx.translate(wposW,wposH);
+  // Triangles drawTriangle(x1,y1,x2,y2,x3,y3,inColor)
   // Child triangle
-  drawTriangle(-sizeR, 0, -sizeR, -sizeR/2, -sizeR/2, 0, "rgba(255, 255, 255, 0.7)");
+  drawTriangle(0, sizeR, sizeR/2,sizeR,0, sizeR/2, "rgba(0, 0, 0, 0.7)");
   // Female
-  drawTriangle(-sizeR, 0, -sizeR, -sizeR, 0, -sizeR, "rgba(255, 0, 0, 0.7)");
+  drawTriangle(sizeR, 0, 0, 0, 0, sizeR, "rgba(255, 0, 0, 0.7)");
+  //drawTriangle(sizeR, 0, sizeR, sizeR, 0, sizeR, "rgba(255, 0, 0, 0.7)");
   // Male
-  drawTriangle(-sizeR, 0, 0, -sizeR, 0, 0, "rgba(200, 25, 25, 0.7)");
+  drawTriangle(0, sizeR, sizeR, 0, sizeR, sizeR, "rgba(200, 25, 25, 0.7)");
   // Older
-  drawTriangle(-sizeR/2, -sizeR, 0, -sizeR, 0, -sizeR/2, "rgba(200, 25, 25, 0.7)");
+  drawTriangle(sizeR, sizeR/2, sizeR, 0, sizeR/2, 0, "rgba(200, 25, 25, 0.7)");
   // Draw setting
-  var xSel = -(1-padSelX)*sizeR;
-  var ySel = -padSelY*sizeR;
-  //drawCircle(-sizeR/2, -sizeR/2);
-  drawCircle(xSel, ySel);
+  var xSel = padSelX*sizeR;
+  var ySel = sizeR - padSelY*sizeR;
+
+  drawCircle(xSel, ySel, 10, "black");
+  drawCircle(xSel, ySel, 8, "white");
 
   // Text
-  drawText("Child", -sizeR -15, 20, undefined, 10);
-  drawText("Old", 0, -sizeR -10, undefined, 10);
-  drawText("Female", -sizeR -15, -sizeR -10, undefined, 10);
-  drawText("Male", -15, 20, undefined, 10);
-  // Reposition
-  canvasCtx.translate(-wposW,-wposH);
+  drawText("Child", 20, sizeR - 20, 15);
+  drawText("Old",  sizeR- 50, 30, 15);
+  drawText("Female", 20, 30, 15);
+  drawText("Male", sizeR- 60, sizeR - 20, 15);
 
-  // If is in square
-  if (xMouse < wposW +10 && xMouse > wposW-sizeR-10 &&
-    yMouse < wposH+10 && yMouse > wposH-sizeR-10){
+  // If is inside square
+  if (xMouse < canvas.offsetLeft + canvas.width && xMouse > canvas.offsetLeft &&
+    yMouse < canvas.offsetTop + canvas.height && yMouse > canvas.offsetTop){
+
+    let xNorm = (xMouse - canvas.offsetLeft)/sizeR;
+    let yNorm = 1 - (yMouse - canvas.offsetTop)/sizeR;
 
     // If mouse is clicked/down
     if (mouseState == 1){
       // Define position (from 0 to 1)
-      drawCircle(xMouse,yMouse, undefined, 'green');
-      var xNorm = 1-(wposW-xMouse)/sizeR;
-      var yNorm = (wposH-yMouse)/sizeR;
+      drawCircle( xNorm*sizeR , (1-yMouse) * sizeR, 10, 'black');
+      drawCircle( xNorm*sizeR , (1-yMouse) * sizeR, 8, 'green');
+
       padSelX = xNorm;
       padSelY = yNorm;
+
       return voiceTransformations(padSelX, padSelY);
 
-    } else
-      drawCircle(xMouse, yMouse);
+    } else {
+      drawCircle( xNorm*sizeR , (1-yNorm) * sizeR, 10, 'black');
+      drawCircle( xNorm*sizeR , (1-yNorm) * sizeR, 8, 'gray');
+    }
   }
+
 }
 
 
@@ -76,7 +81,7 @@ function voiceTransformations(x,y){
   var dist01 = Math.sqrt((x)*(x) + (1-y)*(1-y));
   var dist11 = Math.sqrt((1-x)*(1-x) + (1-y)*(1-y));
   // Distance from diagonal
-  var ddD = (y-x); // from 1 to -1 
+  var ddD = (y-x); // from 1 to -1
 
   // Child
   if (dist00<0.5){
@@ -95,7 +100,7 @@ function voiceTransformations(x,y){
     vocalTractFactor = vocalTractFactor * (Math.abs(ddD) + 1); // Multiplied by two
     pitchFactor /= Math.abs(ddD)*0.5 + 1; // Divided by 1.5
   }
-  
+
   // Old
   // Add vibrato
   // Unvoiced
@@ -121,7 +126,7 @@ function voiceTransformations(x,y){
 
 
 function drawTriangle(x1,y1,x2,y2,x3,y3,inColor){
-  let color = inColor || "rgba(255, 255, 255, 0.5)";
+  let color = inColor || "rgba(0, 0, 0, 0.5)";
 
   canvasCtx.beginPath();
   canvasCtx.moveTo(x1,y1);
@@ -130,13 +135,13 @@ function drawTriangle(x1,y1,x2,y2,x3,y3,inColor){
   canvasCtx.closePath();
   canvasCtx.fillStyle = color;
   canvasCtx.fill();
-} 
+}
 
 
 function drawCircle(x,y, inRadius, inColor){
 
   let radius = inRadius || 6;
-  let color = inColor || "rgba(255,255,255,0.8)";
+  let color = inColor || "rgba(0,0,0,0.8)";
 
   canvasCtx.beginPath();
   canvasCtx.lineWidth = "1";
@@ -148,7 +153,7 @@ function drawCircle(x,y, inRadius, inColor){
 
 function drawText(text, posW, posH, inSize, inColor){
   let size = inSize || 15;
-  let color = inColor || "white";
+  let color = inColor || "black";
   canvasCtx.fillStyle = color;
   canvasCtx.font = size + "px Georgia";
   canvasCtx.fillText(text, posW, posH);
